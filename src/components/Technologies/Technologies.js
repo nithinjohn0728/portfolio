0518baac73b94793fb41eef75e6984f7e2d29115
Nithin John Cherian <nithinjohn0728@gmail.com>
2022-02-07 17:15:11 +0000

    carousel

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,13 +1,44 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiFirebase, DiGit, DiJavascript, DiJavascript1, DiReact, DiZend } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
  <Section id = "tech">
-   <SectionDivider>
+   <SectionDivider/>
      <SectionTitle>Technologies</SectionTitle>
-   </SectionDivider>
+     <SectionText>lorem ipsuim</SectionText>
+     <List>
+      <ListItem>
+        <DiReact size = "3 rem"/>
+        <ListContainer>
+          <ListTitle>FrontEnd</ListTitle>
+          <ListParagraph>
+             Experience with React JS
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+
+      <ListItem>
+        <DiJavascript1 size = "3 rem"/>
+        <ListContainer>
+          <ListTitle>FrontEnd</ListTitle>
+          <ListParagraph>
+             Experience with Javascript
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+
+      <ListItem>
+        <DiGit size = "3 rem"/>
+        <ListContainer>
+          <ListTitle>FrontEnd</ListTitle>
+          <ListParagraph>
+             Experience with Git
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+     </List>
  </Section>
 );
 
